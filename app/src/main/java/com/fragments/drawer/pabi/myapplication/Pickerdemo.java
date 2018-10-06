35a3af23ef383196579cba7f2645e1418efd3e72
Irish Pabi <rubygrace.pabi@gmail.com>
2018-10-06 17:49:05 +0800

    Drawer Fragments

---
+++ app/src/main/java/com/fragments/drawer/pabi/myapplication/Pickerdemo.java
@@ -0,0 +1,39 @@
+package com.fragments.drawer.pabi.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+
+public class Pickerdemo extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private EditText DateEditText;
+    private EditText TimeEditText;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+        DateEditText = layout.findViewById(R.id.date);
+
+
+        Pickerdemo pickerdemo = new Pickerdemo();
+        pickerdemo.setArguments(args);
+        return pickerdemo;
+    }
+}
